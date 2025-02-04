ade9788ee27fb2d2a6c277e96f4eb1b0eef139c9
Alex Davies <53222900+Sinclair8700@users.noreply.github.com>
2025-02-04 20:36:28 +0000

    add check for resize

--- resources/js/text-scroller.js
+++ resources/js/text-scroller.js
@@ -1,14 +1,20 @@
 // if a text box is overflowing add padding on the right to prevent text from being cut off by scroll bar
+function addPaddingToTextBoxes() {
+    document.querySelectorAll('p, h2, h3, h4, h5, h6, span').forEach(textBox => {
+        let text = textBox.textContent;
+        let textHeight = textBox.scrollHeight;
+        let boxHeight = textBox.clientHeight;
+        if (textHeight > boxHeight) {
+            textBox.style.paddingRight = '16px';
+        }
+        else {
+            textBox.style.paddingRight = '0px';
+        }
 
+    });
+}
 
-document.querySelectorAll('p, h2, h3, h4, h5, h6, span').forEach(textBox => {
-    const text = textBox.textContent;
-    const textHeight = textBox.scrollHeight;
-    const boxHeight = textBox.clientHeight;
-    if (textHeight > boxHeight) {
-        textBox.style.paddingRight = '16px';
-    }
-
-});
+addPaddingToTextBoxes()
+window.addEventListener('resize', addPaddingToTextBoxes)
 
 
