edc5aae741019854f4e4a52fc4859b710335a1ef
Alex Davies <53222900+Sinclair8700@users.noreply.github.com>
2025-02-04 01:04:17 +0000

    Better styling + scrollbar styling for if its needed later

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,7 +3,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
             @foreach ($projects as $project)
                 <a href="/projects/{{ $project->slug }}">
-                <div class="bg-black ring-1 ring-white rounded-lg flex flex-col xs:flex-row ">
+                <div class="bg-black ring-1 ring-white rounded-lg flex flex-col xs:flex-row h-full">
                     <div class="order-2 xs:order-first w-full xs:w-1/2 p-4 grow-0">
                         <h2 class="text-lg font-bold">{{ $project->name }}</h2>
 
