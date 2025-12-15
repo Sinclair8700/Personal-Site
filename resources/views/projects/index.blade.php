d75246b56f51e81a43d445bb80047a339bd076d0
Alex Davies <me@alexdavi.es>
2025-12-15 18:33:39 +0000

    fix(projects): mobile view

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,7 +9,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
             @foreach ($projects as $project)
                 <a href="/projects/{{ $project->slug }}">
-                    <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] [&]:p-0 [&:has(.the-image:hover)_.the-text]:w-0 min-w-0">
+                    <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] [&]:p-0 xs:[&:has(.the-image:hover)_.the-text]:w-0 min-w-0">
                         <div class="the-text order-2 xs:order-first w-full xs:w-1/2  flex flex-col min-h-0 transition-[width] duration-600 shrink-0">
                             <h3 class="mx-6 mt-4 text-white mb-1">{{ $project->name }}</h3>
                             <p class="mx-6 mb-4 text-sm max-h-[280px] text-white hyphens-auto overflow-y-auto min-h-0 h-full flex-1 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-w-[10px] scrollbar-thumb-slate-700 scrollbar-track-transparent">
