4a455d6cce9c8d68a6030f58b5a3b097adc7a01b
Alex Davies <me@alexdavi.es>
2025-12-15 18:02:19 +0000

    style(projects): more bubble text padding

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -11,7 +11,7 @@
                 <a href="/projects/{{ $project->slug }}">
                     {{-- <x-bubble class="max-w-full bg-black ring-2 ring-white transition duration-300 focus-within:ring-purple hover:ring-purple rounded-lg flex flex-col xs:flex-row h-full xs:aspect-[2/1]"> --}}
                     <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] [&]:p-0">
-                        <div class="order-2 xs:order-first w-full xs:w-1/2 px-4 py-3 flex flex-col min-h-0">
+                        <div class="order-2 xs:order-first w-full xs:w-1/2 px-6 py-4 flex flex-col min-h-0">
                             <h3 class="text-white mb-1">{{ $project->name }}</h3>
                             <p class="text-sm max-h-[280px] text-white hyphens-auto overflow-y-auto min-h-0 h-full flex-1 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-w-[10px] scrollbar-thumb-slate-700 scrollbar-track-transparent">
                                 {{ $project->description }}</p>
