d996ea35a94f2bb03c14c5a00d4ed790b0298e6d
Alex Davies <me@alexdavi.es>
2025-12-15 18:31:53 +0000

    fix(projects): animation

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -10,9 +10,9 @@
             @foreach ($projects as $project)
                 <a href="/projects/{{ $project->slug }}">
                     <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] [&]:p-0 [&:has(.the-image:hover)_.the-text]:w-0 min-w-0">
-                        <div class="the-text order-2 xs:order-first w-full xs:w-1/2 px-6 py-4 flex flex-col min-h-0 transition-[width] duration-300 shrink-0">
-                            <h3 class="text-white mb-1">{{ $project->name }}</h3>
-                            <p class="text-sm max-h-[280px] text-white hyphens-auto overflow-y-auto min-h-0 h-full flex-1 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-w-[10px] scrollbar-thumb-slate-700 scrollbar-track-transparent">
+                        <div class="the-text order-2 xs:order-first w-full xs:w-1/2  flex flex-col min-h-0 transition-[width] duration-600 shrink-0">
+                            <h3 class="mx-6 mt-4 text-white mb-1">{{ $project->name }}</h3>
+                            <p class="mx-6 mb-4 text-sm max-h-[280px] text-white hyphens-auto overflow-y-auto min-h-0 h-full flex-1 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-w-[10px] scrollbar-thumb-slate-700 scrollbar-track-transparent">
                                 {{ $project->description }}</p>
                         </div>
 
