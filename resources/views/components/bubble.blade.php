596276999544c7b6d31ff60284157f358e2a8d93
Alex Davies <me@alexdavi.es>
2025-12-15 17:39:13 +0000

    style(bubbles): make bubbles expand on hover

--- resources/views/components/bubble.blade.php
+++ resources/views/components/bubble.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'w-full border-2 border-white transition-colors duration-300 focus-within:border-purple hover:border-purple px-6 py-4 rounded-md [&_ul]:text-white [&_ul]:text-[16px] [&_ul]:leading-[24px] [&_ul]:list-disc [&_ul]:list-inside
-    [&_h3]:text-white [&_h3]:text-[24px] [&_h3]:leading-[32px]  [&_h3]:mb-2']) }}>
+<div {{ $attributes->merge(['class' => 'w-full border-2 border-white transition-[colors,_translate] duration-300 focus-within:border-purple hover:border-purple px-6 py-4 rounded-md [&_ul]:text-white [&_ul]:text-[16px] [&_ul]:leading-[24px] [&_ul]:list-disc [&_ul]:list-inside
+    [&_h3]:text-white [&_h3]:text-[24px] [&_h3]:leading-[32px]  [&_h3]:mb-2 hover:scale-[1.25] origin-center']) }}>
     {{ $slot }}
 </div>
