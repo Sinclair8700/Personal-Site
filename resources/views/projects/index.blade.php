f855b56801cd2e54048ce9b543ec069d3951c1a6
Alex Davies <me@alexdavi.es>
2025-12-15 18:27:16 +0000

    style(projects): make image smoothly adjust

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -9,14 +9,14 @@
         <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
             @foreach ($projects as $project)
                 <a href="/projects/{{ $project->slug }}">
-                    <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] xs:w-1/2 [&]:p-0 [&:has(.the-image:hover)_.the-text]:w-0 min-w-0">
-                        <div class="the-text order-2 xs:order-first w-full xs:w-1/2 px-6 py-4 flex flex-col min-h-0 transition-[width] duration-300">
+                    <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] [&]:p-0 [&:has(.the-image:hover)_.the-text]:w-0 min-w-0">
+                        <div class="the-text order-2 xs:order-first w-full xs:w-1/2 px-6 py-4 flex flex-col min-h-0 transition-[width] duration-300 shrink-0">
                             <h3 class="text-white mb-1">{{ $project->name }}</h3>
                             <p class="text-sm max-h-[280px] text-white hyphens-auto overflow-y-auto min-h-0 h-full flex-1 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-w-[10px] scrollbar-thumb-slate-700 scrollbar-track-transparent">
                                 {{ $project->description }}</p>
                         </div>
 
-                        <div class="the-image aspect-square w-full ">
+                        <div class="the-image aspect-square w-full">
                             <img src="{{ asset('storage/projects/' . $project->slug . '/main.png') }}"
                                 alt="{{ $project->name }}"
                                 class="w-full h-full object-cover rounded-t-lg xs:rounded-t-none sm:rounded-r-lg">
