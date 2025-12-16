9c9e3793c6d220a029e820987e6e5c066c9f4fcd
Alex Davies <me@alexdavi.es>
2025-12-16 09:55:55 +0000

    Revert "style(projects): anchor block styling"
    This reverts commit b6781203d3b240cc64b990ec182cced7e53e3e08.

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -1,7 +1,7 @@
 @props([
     'project' => null
 ])
-<a href="/projects/{{ $project->slug ?? null }}" class="block">
+<a href="/projects/{{ $project->slug ?? null }}">
     <x-bubble class="max-w-full flex flex-col xs:flex-row h-full xs:aspect-[2/1] [&]:p-0 xs:[&:has(.the-image:hover)_.the-text]:w-0 xs:[&:has(.the-image:hover)_.the-text]:opacity-0 min-w-0">
         <div class="the-text order-2 xs:order-first w-full xs:w-1/2  flex flex-col min-h-0 transition-[width,_opacity] duration-600 shrink-0">
             <h3 class="mx-6 mt-4 text-white mb-1">{{ $project->name ?? null }}</h3>
