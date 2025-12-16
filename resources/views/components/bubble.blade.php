609983a08e0327049e1d0120c5cf0f1ffbdfaa4a
Alex Davies <me@alexdavi.es>
2025-12-16 11:17:41 +0000

    fix(bubble): border colour transition

--- resources/views/components/bubble.blade.php
+++ resources/views/components/bubble.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->merge(['class' => 'w-full border-2 bg-black border-white transition-[colors,_scale] duration-300 focus-within:border-purple hover:border-purple px-6 py-4 rounded-lg [&_ul]:text-white [&_ul]:text-[16px] [&_ul]:leading-[24px] [&_ul]:list-disc [&_ul]:list-inside
+<div {{ $attributes->merge(['class' => 'w-full border-2 bg-black border-white transition-[border-color,_scale] duration-300 focus-within:border-purple hover:border-purple px-6 py-4 rounded-lg [&_ul]:text-white [&_ul]:text-[16px] [&_ul]:leading-[24px] [&_ul]:list-disc [&_ul]:list-inside
     [&_h3]:text-white [&_h3]:text-[24px] [&_h3]:leading-[32px]  [&_h3]:mb-2 hover:scale-[1.05] [.swiper-slide_&]:scale-100 origin-center']) }}>
     {{ $slot }}
 </div>
