0c3142667b19436c1c9c68c8a2c84c1094331486
Alex Davies <me@alexdavi.es>
2026-01-07 15:35:15 +0000

    chore(sign-in): use nicer fields

--- resources/views/account/sign-in.blade.php
+++ resources/views/account/sign-in.blade.php
@@ -1,6 +1,6 @@
 <form action="{{ route('login') }}" method="POST">
     @csrf
-    <input type="text" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
+    <x-input type="text" name="email" placeholder="Email"/>
+    <x-input type="password" name="password" placeholder="Password"/>
     <button type="submit">Sign In</button>
 </form>
--- resources/views/account/sign-up.blade.php
+++ resources/views/account/sign-up.blade.php
@@ -1,8 +1,8 @@
 <form action="{{ route('register') }}" method="POST">
     @csrf
-    <input type="text" name="name" placeholder="Name">
-    <input type="email" name="email" placeholder="Email">
-    <input type="password" name="password" placeholder="Password">
-    <input type="password" name="password_confirmation" placeholder="Confirm Password">
+    <x-input type="text" name="name" placeholder="Name"/>
+    <x-input type="email" name="email" placeholder="Email"/>
+    <x-input type="password" name="password" placeholder="Password"/>
+    <x-input type="password" name="password_confirmation" placeholder="Confirm Password"/>
     <button type="submit">Sign Up</button>
 </form>
