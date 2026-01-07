10efb59ebb712566133701a08ca316f463b38dcc
Alex Davies <me@alexdavi.es>
2026-01-07 14:07:38 +0000

    fix: user role relationship

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\Relations\HasOne;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
@@ -46,8 +46,8 @@ class User extends Authenticatable
         ];
     }
 
-    protected function role() : HasOne  {
-        return $this->hasOne(Role::class);
+    protected function role() : BelongsTo  {
+        return $this->belongsTo(Role::class);
     }
 
     public function hasRole($role)
