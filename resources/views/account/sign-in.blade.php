77ac09fd606b5736ae7e2e6abdc26992a225c7c9
Alex Davies <me@alexdavi.es>
2026-01-07 15:37:44 +0000

    chore(login): improve page layout

--- resources/views/account/sign-in.blade.php
+++ resources/views/account/sign-in.blade.php
@@ -1,6 +1,15 @@
-<form action="{{ route('login') }}" method="POST">
-    @csrf
-    <x-input type="text" name="email" placeholder="Email"/>
-    <x-input type="password" name="password" placeholder="Password"/>
-    <button type="submit">Sign In</button>
-</form>
+<x-page :title="$title">
+    <x-content class="h-full flex flex-col gap-6 py-6">
+
+        <x-slot name="leftGutter" class="snow">
+        </x-slot>
+        <x-slot name="rightGutter" class="snow">
+        </x-slot>
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <x-input type="text" name="email" placeholder="Email"/>
+            <x-input type="password" name="password" placeholder="Password"/>
+            <button type="submit">Sign In</button>
+        </form>
+    </x-content>
+</x-page>
--- resources/views/account/sign-up.blade.php
+++ resources/views/account/sign-up.blade.php
@@ -1,8 +1,18 @@
-<form action="{{ route('register') }}" method="POST">
-    @csrf
-    <x-input type="text" name="name" placeholder="Name"/>
-    <x-input type="email" name="email" placeholder="Email"/>
-    <x-input type="password" name="password" placeholder="Password"/>
-    <x-input type="password" name="password_confirmation" placeholder="Confirm Password"/>
-    <button type="submit">Sign Up</button>
-</form>
+<x-page :title="$title">
+    <x-content class="h-full flex flex-col gap-6 py-6">
+
+        <x-slot name="leftGutter" class="snow">
+        </x-slot>
+        <x-slot name="rightGutter" class="snow">
+        </x-slot>
+
+        <form action="{{ route('register') }}" method="POST">
+            @csrf
+            <x-input type="text" name="name" placeholder="Name"/>
+            <x-input type="email" name="email" placeholder="Email"/>
+            <x-input type="password" name="password" placeholder="Password"/>
+            <x-input type="password" name="password_confirmation" placeholder="Confirm Password"/>
+            <button type="submit">Sign Up</button>
+        </form>
+    </x-content>
+</x-page>
