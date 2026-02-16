a6f15f076033b8e54d4a9c39fdb6f5d9410c88f7
Alex Davies <me@alexdavi.es>
2026-02-16 12:54:03 +0000

    wip: update form

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,6 +1,14 @@
-<form {{ !isset($method) || $method == 'GET' ? 'method=GET': 'method=POST' }} {{ $attributes->merge(['class' => 'flex flex-col gap-4 max-w-4xl ']) }}>
+@props([
+    'method' => 'POST',
+])
+@php
+    $formMethod = in_array(strtoupper($method), ['GET', 'POST']) ? strtoupper($method) : 'POST';
+@endphp
+<form method="{{ $formMethod }}" {{ $attributes->except('method')->merge(['class' => 'flex flex-col gap-4 max-w-4xl']) }}>
     @csrf
-    @method($method ?? 'GET')
+    @if($formMethod !== 'GET')
+        @method($method)
+    @endif
     {{ $slot }}
 </form>
 
