3cab664ced161ac40c345a0c9feed16b2f02d670
Alex Davies <me@alexdavi.es>
2026-02-16 11:40:30 +0000

    fix(project): use escaped description

--- resources/views/components/project.blade.php
+++ resources/views/components/project.blade.php
@@ -7,7 +7,7 @@
         <div class="the-text order-2 xs:order-first w-full xs:w-1/2  flex flex-col min-h-0 transition-[width,_opacity] duration-600 shrink-0">
             <h3 class="mx-6 mt-4 text-white mb-1">{{ $project->name ?? null }}</h3>
             <p class="mx-6 mb-4 text-sm max-h-[280px] text-white hyphens-auto overflow-y-auto min-h-0 h-full flex-1 scrollbar-thumb-rounded-full scrollbar-track-rounded-full scrollbar scrollbar-w-[10px] scrollbar-thumb-slate-700 scrollbar-track-transparent">
-                {{ $project->description ?? null }}</p>
+                {{ $project->escapedDescription ?? null }}</p>
         </div>
 
         <div class="the-image aspect-square w-full min-w-0">
