14d0d184abc67a1ffec68270b7ccf25e4038d3a2
Alex Davies <me@alexdavi.es>
2026-02-17 16:59:06 +0000

    fix(contact-form): input

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -8,7 +8,7 @@
         
         <x-form method="POST" action="{{ route('contact.store') }}">
             <x-input name="email_address" type="text">Email</x-input>
-            <x-inpu name="message" type="textarea">Message</x-input>
+            <x-input name="message" type="textarea">Message</x-input>
             <x-button>Send</x-button>
         </x-form>
     </x-content>
