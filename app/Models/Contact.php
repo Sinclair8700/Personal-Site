9206bebe03949e335a63f48cd15543a6c5d647d7
Alex Davies <me@alexdavi.es>
2026-02-17 16:52:40 +0000

    fix: added contact table name to model

--- app/Models/Contact.php
+++ app/Models/Contact.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
+    protected $table = 'contact';
+
     protected $fillable = [
         'message',
         'email_address',
