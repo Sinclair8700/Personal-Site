0bf07c52147901a65c268eb01b24efb1a7baef54
Alex Davies <me@alexdavi.es>
2026-02-17 16:09:14 +0000

    wip: contact

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Contact;
+use Illuminate\Http\Request;
+
+class ContactController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('contact.index');
+    }
+
+
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'message|max:511|min:10',
+            'email_address|email|max:255',
+        ]);
+
+        (new Contact($request->all()))->save();
+
+        return redirect()->back(200);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Contact $contact)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Contact $contact)
+    {
+        //
+    }
+}
---
+++ resources/views/contact/index.blade.php
@@ -0,0 +1,15 @@
+<x-page :title="$title">
+    <x-content type="wide" class="py-6">
+        
+        <x-slot name="leftGutter" class="snow">
+        </x-slot>
+        <x-slot name="rightGutter" class="snow">
+        </x-slot>
+        
+        <x-form method="POST" action="{{ route('contact.create') }}">
+            <x-input>Email</x-input>
+            <x-input>Message</x-input>
+            <x-button>Send</x-button>
+        </x-form>
+    </x-content>
+</x-page>
